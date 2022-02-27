9aa278649e0adef768dbafface020c0897faef4a
lights <87731955+prosodia@users.noreply.github.com>
2022-02-27 14:47:38 -0500

    Create blog.js

---
+++ models/blog.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { marked } = require("marked");
+const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
+
+/* image: { default: '' } DOES NOT WORK. */
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    snippet: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false,
+        default: 'https://placehold.jp/1080x700.png' 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+}, { timestamps: true });
+
+blogSchema.pre('validate', function(next) {
+    if (this.body) {
+        this.body = dompurify.sanitize(marked.parse(this.body))
+    };
+
+    next();
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
